5d7297c4900002797851d18be2f8ac661b5c9121
Maddie Cusimano <mcusi@mit.edu>
2021-09-29 23:09:34 -0400

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,5 +4,5 @@
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
-c.NotebookApp.password="sha1:bca9422a4bbf:6b2c319684b3b95d30245b6b3b9ee307245032bb"
+c.NotebookApp.password="argon2:$argon2id$v=19$m=10240,t=10,p=8$G0U7rndm7r+moe0a/C9ADw$648QHqduGiEEDz7Ru5EKPg"
 c.NotebookApp.token=""
